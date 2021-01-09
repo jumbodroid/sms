6b03b4da7c246c356c734145c2c88c6a1b368468
Alois Odhiambo <rayalois22@gmail.com>
2021-01-09 22:48:38 +0300

    AfricasTalking Gateway: Added shortcode + callbacks

--- src/Config/sms.php
+++ src/Config/sms.php
@@ -27,7 +27,12 @@ return [
             'apikey'        => env('AFRICASTALKING_API_KEY', ''),
             'username'      => env('AFRICASTALKING_API_USERNAME', 'sandbox'),
             'alphanumeric'  => env('AFRICASTALKING_API_ALPHANUMERIC', ''),
+            'shortcode'     => env('AFRICASTALKING_API_SHORTCODE', ''),
             'class'         => 'Jumbodroid\Sms\Gateways\AfricasTalking',
+            'deliveryReportsCallback'           => env('AFRICASTALKING_API_DELIVERY_REPORTS_CALLBACK', ''),
+            'incomingMessagesCallback'          => env('AFRICASTALKING_API_INCOMING_MESSAGES_CALLBACK', ''),
+            'bulkSmsOptOutCallback'             => env('AFRICASTALKING_API_BULK_SMS_OPT_OUT_CALLBACK', ''),
+            'subscriptionNotificationsCallback' => env('AFRICASTALKING_API_SUBSCRIPTION_NOTIFICATIONS_CALLBACK', ''),
         ],
     ],
 ];
